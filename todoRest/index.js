867aae036ce815132902838efe38177fcad8c227
Guilherme Prado <guilhermeprado2112@gmail.com>
2018-03-13 18:49:37 -0300

    find item

--- todoRest/index.js
+++ todoRest/index.js
@@ -5,19 +5,39 @@ var app = express()
 app.use(helmet())
 const PORT = process.env.PORT || 3000
 
-var todo = [{
-  name: 'olaf',
-  age: 15,
-  sex: 'M'
+var todos = [{
+  id: 1,
+  description: "Meer mom for launch",
+  completed: false
 }, {
-  name: 'teste',
-  age: 19,
-  sex: 'F'
-}]
+  id: 2,
+  description: "Go to market",
+  completed: false
+}, {
+  id: 3,
+  description: "Go to college",
+  completed: true
+}
+]
 
 app.get('/todos', function (req, res) {
   console.log('REQUEST => ', req.connection.remoteAddress, ' Date => ', new Date().toString())
-  res.send(JSON.stringify(todo))
+  res.send(JSON.stringify(todos))
+})
+
+app.get('/todos/:id', function (req, res) {
+  var todoId = parseInt(req.params.id)
+  var todoMached;
+  todos.forEach(function (unique) {
+    if (unique.id === todoId) {
+      todoMached = unique
+    }
+  })
+  if (todoMached) {
+    res.json(todoMached)
+  } else {
+    res.status(404).send()
+  }
 })
 
 app.listen(PORT, function () {
