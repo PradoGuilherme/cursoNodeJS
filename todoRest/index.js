b2a6c352acbd176c685cb4a5acf28cd68a5ffb69
Guilherme Nunes <guilhermeprado2112@gmail.com>
2018-03-15 12:57:40 -0300

    adicionado modulo underscore para validar campos

--- todoRest/index.js
+++ todoRest/index.js
@@ -1,23 +1,32 @@
-var express = require('express')
+const express = require('express')
 const helmet = require('helmet')
+const bodyParser = require('body-parser')
+const _ = require('underscore')
 var app = express()
-
+var nextTodoId = 1
 app.use(helmet())
+app.use(bodyParser.json())
+
 const PORT = process.env.PORT || 3000
 
-var todo = [{
-  name: 'olaf',
-  age: 15,
-  sex: 'M'
-}, {
-  name: 'teste',
-  age: 19,
-  sex: 'F'
-}]
+var todos = []
 
 app.get('/todos', function (req, res) {
   console.log('REQUEST => ', req.connection.remoteAddress, ' Date => ', new Date().toString())
-  res.send(JSON.stringify(todo))
+  res.send(JSON.stringify(todos))
+})
+
+app.post('/todos', function (req, res) {
+  var body = _.pick(req.body, 'description', 'completed')
+  if (!_.isBoolean(req.body.completed) || !_.isString(req.body.description)) {
+    return res.sendStatus(400)
+  }
+
+  body.description = body.description.trim()
+  body.id = nextTodoId++
+  todos.push(body)
+
+  res.send(body)
 })
 
 app.listen(PORT, function () {
