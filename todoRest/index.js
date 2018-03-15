dbda14dc6fca99c9a9cfd8fa23aae61560f64051
Guilherme Nunes <guilhermeprado2112@gmail.com>
2018-03-15 15:45:08 -0300

    alterações para deletar

--- todoRest/index.js
+++ todoRest/index.js
@@ -13,7 +13,7 @@ var todos = []
 
 app.get('/todos', function (req, res) {
   console.log('REQUEST => ', req.connection.remoteAddress, ' Date => ', new Date().toString())
-  res.send(JSON.stringify(todos))
+  res.json(todos)
 })
 
 app.post('/todos', function (req, res) {
@@ -31,8 +31,20 @@ app.post('/todos', function (req, res) {
 
 app.get('/todos/:id', function (req, res) {
   const todoId = parseInt(req.params.id)
-  var todoMached
-  todoMached = _.findWhere(todos, {id: todoId})
+  var todoMached = _.findWhere(todos, {id: todoId})
+  if (todoMached) {
+    res.json(todoMached)
+  } else {
+    res.status(404).send('Not find to do')
+  }
+})
+
+app.delete('/todos/:id', function (req, res) {
+  const todoId = parseInt(req.params.id)
+  var todoMached = _.findWhere(todos, {id: todoId})
+  todoMached = _.without(todos, todoMached)
+  todos = todoMached
+  console.log(todoMached)
   if (todoMached) {
     res.json(todoMached)
   } else {
